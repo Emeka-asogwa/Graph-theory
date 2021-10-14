00205d4335d4a5befb6766bdec482ef25a69f5ce
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-10-14 18:55:37 -0400

    ✍🏽 Add depthFirstPrint

---
+++ graph_theory.js
@@ -0,0 +1,22 @@
+const graph = {
+  a: ["c", "b"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+const depthFirstPrint = (graph, source) => {
+  const stack = [source];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current);
+
+    // for (let neighbor of graph[current]) {
+    // stack.push(neighbor);
+    // }
+  }
+};
+
+depthFirstPrint(graph, "a");

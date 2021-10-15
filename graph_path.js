41bbd0caaccca339cb41492d5e990022310d736b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-10-14 21:49:22 -0400

    Shortest path

--- graph_path.js
+++ graph_path.js
@@ -7,14 +7,29 @@ const graph = {
   f: [],
 };
 
+// ------  Iterative method ------
+// const hashPath = (graph, src, dst) => {
+//   const queue = [src];
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+//     if (current === dst) return true;
+//     for (let neighbor of graph[current]) {
+//       queue.push(neighbor);
+//     }
+//   }
+//   return false;
+// };
+
+//----  Recursive method -----
 const hashPath = (graph, src, dst) => {
-  const queue = [src];
-  while (queue.length > 0) {
-    const current = queue.shift();
-    if (current === dst) return true;
-    for (let neighbor of graph[current]) {
-      queue.push(neighbor);
+  if (src === dst) return true;
+
+  for (let neighbor of graph[src]) {
+    if (hashPath(graph, neighbor, dst) === true) {
+      return true;
     }
   }
   return false;
 };
+
+console.log(hashPath(graph, "b", "e"));
